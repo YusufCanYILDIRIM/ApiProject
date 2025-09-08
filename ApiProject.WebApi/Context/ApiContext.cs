3505f656404013b3eb44862c9795967c5997152a
Yusuf Can YILDIRIM <yusufyildirimcan@outlook.com>
2025-09-08 16:27:15 +0300

    Add project files.

---
+++ ApiProject.WebApi/Context/ApiContext.cs
@@ -0,0 +1,28 @@
+﻿using ApiProject.WebApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProject.WebApi.Context
+{
+    public class ApiContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                    "Server=ASUS\\SQLEXPRESS;Initial Catalog=ApiYummyDb;Integrated Security=True;TrustServerCertificate=True;");
+            }
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Chef> Chefs { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
